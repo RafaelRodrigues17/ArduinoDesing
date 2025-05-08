efc2f31d86bbfa007ed51ca8eae08b5cdcbaa31d
Rodrigo301239 <rodrigoifpr1@gmail.com>
2025-05-08 17:01:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, flash
-import database
+from flask import Flask, render_template, flash, session, request, redirect, url_for
+from database import Banco
 
-database.conectar()
+banco = Banco()
 
 app = Flask(__name__)
 app.secret_key = "chave_muito_segura"
@@ -15,13 +15,48 @@ def home():
     return render_template('home.html')
 
 @app.route('/led')
-def led(ip):
-    if database.acender(ip) == True:
-        return render_template('led.html')
+def led():
+    return render_template('led.html')
+
+@app.route('/acender_led', methods=['POST'])
+def acender_led():
+    ip = request.form.get('ip')
+
+    if request.method == "POST":
+        if banco.acender(ip) == True:
+            return redirect(url_for('led'))
+        
+        else:
+            flash("ip nao encontrado")
+            return redirect(url_for('led'))
+
+@app.route('/cadastrar',methods = ["GET","POST"])
+def cadastrar():
+    form = request.form
+    if Banco.cadastro(form) == True:
+        return render_template('index.html')
     
     else:
-        flash("ip nao encontrado")
-        return render_template('led.html')
+        return ("erro1")
+
+
+@app.route('/login',methods = ['GET','POST'])   
+def login():
+    form =  request.form
+
+    if request.method == 'POST':
+        email = form['email']
+        senha = form['senha']
+
+        if Banco.login(form) == True:
+            return redirect(url_for('home'))
+
+        else:
+            flash("senha ou email incorreto")
+        
+    return render_template('index.html')
+
+        
     
 
 
