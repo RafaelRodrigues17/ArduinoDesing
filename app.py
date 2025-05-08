4d448d260466b2448910a5f93ed475e7e36a5c59
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2025-05-08 16:55:22 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash, session
+import database
 
 app = Flask(__name__)
 app.secret_key = "chave_muito_segura"
@@ -14,6 +15,29 @@ def home():
 @app.route('/led')
 def led():
     return render_template('led.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        form = request.form
+        if database.login(form) == True:
+            session['usuario'] = form['email'] 
+            return redirect(url_for('home'))
+        else:
+            return "Ocorreu um erro ao fazer o login do usuário"  
+    else:
+        return render_template('login.html')  
+
+@app.route('/cadastro', methods=["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+        form = request.form 
+        if database.criar_usuario(form) == True:
+            return render_template('login.html') 
+        else:
+            return "Ocorreu um erro ao cadastrar usuário" 
+    else:
+        return render_template('cadastro.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
