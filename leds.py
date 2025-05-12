9b879df9bfa2970d3e2b72d9a407b20420af671f
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2025-05-12 17:01:38 -0300

    Atualização

---
+++ leds.py
@@ -0,0 +1,42 @@
+import socket
+import time
+ 
+def principal(ip_usuario, estado_led):
+    
+    HOST = ["192.168.1.177", "192.168.1.178", "192.168.1.105"]
+    PORT = 5000
+    MAX_TENTATIVAS = 30
+    TIMEOUT = 5
+
+    
+    if ip_usuario not in HOST:
+        print("Dispositivo inválido! Programa encerrado.")
+        return False  
+
+    
+    if estado_led == 1:
+        msg = b"ligar\n"
+    else:
+        msg = b"desligar\n"
+        
+   
+    ip = HOST.index(ip_usuario)
+
+    
+    for tentativa in range(1, MAX_TENTATIVAS + 1):
+        try:
+            print(f"Tentando abrir conexão com IP: {HOST[ip]}")
+            s = socket.create_connection((HOST[ip], PORT), timeout=2)
+            print("Conexão aberta")
+            s.settimeout(TIMEOUT)
+            s.sendall(msg)  
+            print(f"Comando executado: {s.recv(16).decode()}")
+            break
+        except socket.timeout:
+            print(f"Tempo esgotado (tentativa {tentativa}/{MAX_TENTATIVAS}).")
+        except OSError as e:
+            print(f"Falha de rede ({e}) (tentativa {tentativa}/{MAX_TENTATIVAS}).")
+        finally:
+            if s:
+                s.close()
+                print("Conexão fechada.\n")
