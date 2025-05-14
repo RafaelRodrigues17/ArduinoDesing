f9c917472e8299f7016cc593ada0fdd59502df8a
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2025-05-14 17:05:39 -0300

    Melhorias no arquivo led.html

--- database.py
+++ database.py
@@ -5,10 +5,11 @@ from leds import principal
 class Banco:
     def __init__(self):
         self.__conexao = mysql.connector.connect(
-              host='localhost',
-              user='root',
-              password='',
-              database='test'
+            host = 'paparella.com.br',
+            user = 'paparell_aluno_4',
+            password = '@Senai2025',
+            database = 'paparell_python'
+
         )
         self.__cursor = self.__conexao.cursor()
         self.criar_tabela()
