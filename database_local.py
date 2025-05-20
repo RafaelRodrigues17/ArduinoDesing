d2316d87c0a3a26dd52b65d9dee219c0e7f6c754
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-05-20 16:51:38 -0300

    buzzer e pir

--- database_local.py
+++ database_local.py
@@ -12,7 +12,7 @@ class BancoLocal:
         self.cursor.execute ("""create table if not exists pir (id integer primary key autoincrement, movimento text, nome text, hora text, data text)""")
         self.cursor.execute ("""create table if not exists fotoresistor (id integer primary key autoincrement, luminosidade integer, nome text)""")
         self.cursor.execute ("""create table if not exists buzzer (id integer primary key autoincrement, som text, nome text, hora text, data text)""")
-        self.cursor.execute ("""create table if not exists touch (id integer primary key autoincremnt, status text, nome, text, hora text, data text)""")
+        self.cursor.execute ("""create table if not exists touch (id integer primary key autoincrement, status text, nome, text, hora text, data text)""")
         self.conexao.commit ()
         
     def dados_ultrassonico (self):
